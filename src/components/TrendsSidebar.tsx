94dbb78a0123231c47e0839a10a4b5295ebbaf60
HamzaIshaq <156201157+Hamzaisadev@users.noreply.github.com>
2024-10-29 14:21:05 +0500

    Add User ToolTip

--- src/components/TrendsSidebar.tsx
+++ src/components/TrendsSidebar.tsx
@@ -11,6 +11,7 @@ import UserAvatar from "./ui/UserAvatar";
 
 import FollowButton from "./FollowButton";
 import { getUserDataSelect } from "@/lib/type";
+import UserTooltip from "./UserToolotip";
 
 export default function TrendsSidebar() {
   return (
@@ -33,11 +34,11 @@ async function WhoToFollow() {
       NOT: {
         id: user.id,
       },
-      followers:{
-        none:{
-          followerId : user.id
-        }
-      }
+      followers: {
+        none: {
+          followerId: user.id,
+        },
+      },
     },
     select: getUserDataSelect(user.id),
     take: 5,
@@ -48,31 +49,31 @@ async function WhoToFollow() {
       <div className="text-xl font-bold">Who to follow</div>
       {usersToFollow.map((user) => (
         <div key={user.id} className="flex items-center justify-between gap-3">
-          <Link
-            href={`/users/${user.username}`}
-            className="flex items-center gap-3"
-          >
-            <UserAvatar avatarUrl={user.avatarUrl} className="flex-none" />
-            <div>
-              <p className="line-clamp-1 break-all font-semibold hover:underline">
-                {user.displayName}
-              </p>
-              <p className="line-clamp-1 break-all text-muted-foreground">
-                @{user.username}
-              </p>
-            </div>
-          </Link>
-          <FollowButton 
-          userId={user.id}
-          initialState={{
-            followers: user._count.followers,
-            isFollowedByUser: user.followers.some(
-              ({followerId}) => followerId === user.id
-            )
-          }}
-          
+          <UserTooltip user={user}>
+            <Link
+              href={`/users/${user.username}`}
+              className="flex items-center gap-3"
+            >
+              <UserAvatar avatarUrl={user.avatarUrl} className="flex-none" />
+              <div>
+                <p className="line-clamp-1 break-all font-semibold hover:underline">
+                  {user.displayName}
+                </p>
+                <p className="line-clamp-1 break-all text-muted-foreground">
+                  @{user.username}
+                </p>
+              </div>
+            </Link>
+          </UserTooltip>
+          <FollowButton
+            userId={user.id}
+            initialState={{
+              followers: user._count.followers,
+              isFollowedByUser: user.followers.some(
+                ({ followerId }) => followerId === user.id,
+              ),
+            }}
           />
-          
         </div>
       ))}
     </div>
@@ -118,7 +119,7 @@ async function TrendingTopics() {
               {hashtag}
             </p>
             <p className="text-sm text-muted-foreground">
-              {formatNumber(count)} {count ===  1 ? "post" : "posts"}
+              {formatNumber(count)} {count === 1 ? "post" : "posts"}
             </p>
           </Link>
         );
--- src/components/posts/posts.tsx
+++ src/components/posts/posts.tsx
@@ -1,44 +1,57 @@
 "use client";
 
-
-import { postData } from "@/lib/type"
-import UserAvatar from "../ui/UserAvatar"
-import Link from "next/link"
-import { formatRelativeDate } from "@/lib/utils"
-import { useSession } from "@/app/(main)/SessionProvider"
+import { postData } from "@/lib/type";
+import UserAvatar from "../ui/UserAvatar";
+import Link from "next/link";
+import { formatRelativeDate } from "@/lib/utils";
+import { useSession } from "@/app/(main)/SessionProvider";
 import PostMoreButton from "./PostMoreButton";
+import Linkify from "../Linkify";
+import UserTooltip from "../UserToolotip";
 
-
-interface PostProps{
-    post: postData
-
+interface PostProps {
+  post: postData;
 }
 
-export default function Post ({post}: PostProps){
-    const { user } = useSession()
+export default function Post({ post }: PostProps) {
+  const { user } = useSession();
 
-
-    return <article className='group/post space-y-3 rounded-2xl bg-card p-5 shadow-sm'>
-        <div className="flex justify-between gap-3">
-            <div className='flex flex-wrap gap-3'>
-                <Link href={`/users/${post.user.username}`}>
-                    <UserAvatar avatarUrl={post.user.avatarUrl} />
-                </Link>
-                <div>
-                    <Link href={`/users/${post.user.username}`}
-                    className="block font-medium hover:underline">
-                        {post.user.displayName}
-                    </Link>
-                    <Link href={`/posts/${post.id}`}
-                    className="block text-sm text-muted-foreground hover:underline">
-                        {formatRelativeDate(post.createdAt)}
-                    </Link>
-                </div>
-            </div>
-            {post.user.id === user.id && (
-                <PostMoreButton post={post} className="opacity-0 transition-opacity group-hover/post:opacity-100" />
-            )}
+  return (
+    <article className="group/post space-y-3 rounded-2xl bg-card p-5 shadow-sm">
+      <div className="flex justify-between gap-3">
+        <div className="flex flex-wrap gap-3">
+          <UserTooltip user={post.user}>
+            <Link href={`/users/${post.user.username}`}>
+              <UserAvatar avatarUrl={post.user.avatarUrl} />
+            </Link>
+          </UserTooltip>
+          <div>
+            <UserTooltip user={post.user}>
+              <Link
+                href={`/users/${post.user.username}`}
+                className="block font-medium hover:underline"
+              >
+                {post.user.displayName}
+              </Link>
+            </UserTooltip>
+            <Link
+              href={`/posts/${post.id}`}
+              className="block text-sm text-muted-foreground hover:underline"
+            >
+              {formatRelativeDate(post.createdAt)}
+            </Link>
+          </div>
         </div>
-        <div className='whitespace-pre-line break-words'>{post.content}</div>
-    </article> 
+        {post.user.id === user.id && (
+          <PostMoreButton
+            post={post}
+            className="opacity-0 transition-opacity group-hover/post:opacity-100"
+          />
+        )}
+      </div>
+      <Linkify>
+        <div className="whitespace-pre-line break-words">{post.content}</div>
+      </Linkify>
+    </article>
+  );
 }
